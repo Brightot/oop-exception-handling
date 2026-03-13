e521cde63f3714d1b3a73437a1f9616db4ab8c74
D00274428 <D00274427@student.dkit.ie>
2026-03-13 00:29:06 +0000

    Created basic main program structure

--- student-grades-assignment/main.py
+++ student-grades-assignment/main.py
@@ -1,8 +1,8 @@
-# importing the Student class
 
+# here I am importing the Student class
 from student import Student
 
-# dictionary to store students
-# key = student ID
+# dictionary to store all students
+# key = student id
 # value = student object
 students = {}
