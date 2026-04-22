a681944e554104fbce4cccb7829221d297a1795d
D00274428 <D00274427@student.dkit.ie>
2026-04-22 20:46:01 +0100

    Added method to assign ticket to agent

--- ticket-layered-assignment/ticket_service.py
+++ ticket-layered-assignment/ticket_service.py
@@ -11,3 +11,18 @@ class TicketService:
         # return all unassigned tickets
     def get_unassigned_tickets(self):
         return self._unassigned_tickets
+    
+    def assign_ticket(self, ticket, agent):
+
+        if ticket in self._unassigned_tickets:
+            self._unassigned_tickets.remove(ticket)
+
+            # if agent not already in dictionary, create list
+            if agent not in self._assigned_tickets:
+                self._assigned_tickets[agent] = []
+
+            self._assigned_tickets[agent].append(ticket)
+
+            return True
+
+        return False
