259f13b93b166caf8b258b74a52acded28fbaedf
D00274428 <D00274427@student.dkit.ie>
2026-04-22 20:56:16 +0100

    Added method to assign next available ticket

--- ticket-layered-assignment/ticket_service.py
+++ ticket-layered-assignment/ticket_service.py
@@ -35,3 +35,21 @@ class TicketService:
         if agent in self._assigned_tickets:
             return self._assigned_tickets[agent]
         return []
+    
+    def assign_next_ticket(self, agent):
+
+        if len(self._unassigned_tickets) > 0:
+
+            ticket = self._unassigned_tickets.pop(0)
+
+            # if agent not in dictionary, create list
+            if agent not in self._assigned_tickets:
+                self._assigned_tickets[agent] = []
+
+            # assign ticket
+            self._assigned_tickets[agent].append(ticket)
+
+            return ticket
+
+        # if no tickets left
+        return None
