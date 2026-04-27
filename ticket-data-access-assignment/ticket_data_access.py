4495d1a9a2329b64420fd2d929acff020cbe0b3d
D00274428 <D00274427@student.dkit.ie>
2026-04-27 12:30:35 +0100

    Added method to parse ticket data from file

--- ticket-data-access-assignment/ticket_data_access.py
+++ ticket-data-access-assignment/ticket_data_access.py
@@ -16,3 +16,33 @@ class TicketDataAccess:
         file.close()
 
         return lines
+    
+    # this method will process lines into data structures
+def get_ticket_data(self, filename):
+
+    lines = self.read_file(filename)
+
+    unassigned = []
+    assigned = {}
+
+    for line in lines:
+
+        parts = line.strip().split(",")
+
+        if len(parts) < 2:
+            continue
+
+        ticket = parts[0]
+        agent = parts[1]
+
+        # if no agent then unassigned it 
+        if agent == "":
+            unassigned.append(ticket)
+
+        else:
+            if agent not in assigned:
+                assigned[agent] = []
+
+            assigned[agent].append(ticket)
+
+    return unassigned, assigned
