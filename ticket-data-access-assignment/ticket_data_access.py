a50f805eef4cf8d11a041d325628a83f2f71dbdb
D00274428 <D00274427@student.dkit.ie>
2026-04-27 12:06:39 +0100

    Added method to read ticket file

--- ticket-data-access-assignment/ticket_data_access.py
+++ ticket-data-access-assignment/ticket_data_access.py
@@ -2,3 +2,17 @@ class TicketDataAccess:
 
     def __init__(self):
         pass
+# this class will handle reading ticket data from file
+class TicketDataAccess:
+
+    def __init__(self):
+        pass
+    def read_file(self, filename):
+
+        file = open(filename, "r")
+
+        lines = file.readlines()
+
+        file.close()
+
+        return lines
